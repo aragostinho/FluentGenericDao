3e39edc4c1ca5fc1c159bddc2fe432ceb95d6c0c
cassio123 <cassio.henriqueuru@gmail.com>
2015-06-11 09:40:44 -0300

    Test

--- FluentGenericDao.Console/Task/ListResponsible.cs
+++ FluentGenericDao.Console/Task/ListResponsible.cs
@@ -15,7 +15,16 @@ namespace FluentGenericDao.TaskConsole.Task
             IEnumerable<Responsible> responsible = BusinessFactory.Responsible.Get(x => x.Id != 0);
             foreach (var item in responsible)
             {
+                Console.WriteLine("============================================");
                 Console.WriteLine("Responsible Name:{0}", item.Name);
+
+                Console.Write("Task");
+                foreach (var task in item.Task)
+                {
+                    Console.Write("Task Descrition: {0}", task.Description);
+                }
+
+                Console.WriteLine("=============================================");
             }
         }
 
--- FluentGenericDao.Console/Task/SearchTask.cs
+++ FluentGenericDao.Console/Task/SearchTask.cs
@@ -11,10 +11,19 @@ namespace FluentGenericDao.TaskConsole.Task
     {
         public override void Execute(string[] args)
         {
-            Console.WriteLine("enter the task name. (Criteria)");
+            Console.WriteLine("enter the task name or empty to search all. (Criteria)");
             string name = Console.ReadLine();
 
             var list = BusinessFactory.Task.ListTaskName(name);
+
+            if (list.Count() < 1)
+            {
+                Console.WriteLine("==========================================================");
+
+                Console.WriteLine("It was not found to search results!");
+
+                Console.WriteLine("==========================================================");
+            }
 
             foreach (var item in list.ToList())
 	        {
